f642dcb781f039f45e4431d853f6a7f11649f172
Vladimir Pavlovski <vpavlovskiy@megalabs.ru>
2014-09-24 17:26:35 +0400

    little more asserts

--- transactions/postgre/src/main/java/ru/antivoland/sandbox/transactions/postgre/Application.java
+++ transactions/postgre/src/main/java/ru/antivoland/sandbox/transactions/postgre/Application.java
@@ -3,12 +3,13 @@ package ru.antivoland.sandbox.transactions.postgre;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.CannotSerializeTransactionException;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.ArrayList;
@@ -21,27 +22,22 @@ import java.util.concurrent.Future;
 @EnableAutoConfiguration
 @EnableTransactionManagement
 @ComponentScan
-@Configuration
+@Component
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-    private static final ExecutorService executor = Executors.newFixedThreadPool(5);
+    private final ExecutorService executor = Executors.newFixedThreadPool(10);
 
-    public static void main(String[] args) throws Exception {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    @Autowired
+    PersonService personService;
 
-        final PersonService personService = ctx.getBean(PersonService.class);
-        personService.addAll("Alice", "Bob", "Carol");
-        Assert.assertEquals(3, personService.findAll().size());
-
-        log.info(personService.findAll().toString());
-
+    private List<Future> submitPersonUpdates(final String id, int count) {
         List<Future> futures = new ArrayList<Future>();
-        for (int i = 0; i < 50; ++i) {
+        for (int i = 0; i < count; ++i) {
             futures.add(executor.submit(new Callable<Object>() {
                 @Override
                 public Object call() throws Exception {
                     try {
-                        personService.updateState("Bob");
+                        personService.nextState(id);
                     } catch (CannotSerializeTransactionException e) {
                         return call();
                     }
@@ -49,8 +45,30 @@ public class Application {
                 }
             }));
         }
+        return futures;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        final PersonService personService = ctx.getBean(PersonService.class);
+        personService.addAll("Alice", "Bob", "Carol");
+        Assert.assertEquals(3, personService.findAll().size());
+        log.info(personService.findAll().toString());
+
+        long startMillis = System.currentTimeMillis();
+        Application application = ctx.getBean(Application.class);
+        List<Future> futures = new ArrayList<Future>();
+        futures.addAll(application.submitPersonUpdates("Alice", 40));
+        futures.addAll(application.submitPersonUpdates("Bob", 30));
+        futures.addAll(application.submitPersonUpdates("Carol", 50));
 
         for (Future future : futures) future.get();
         log.info(personService.findAll().toString());
+        log.info(String.format("Elapsed time: %sms", System.currentTimeMillis() - startMillis));
+
+        Assert.assertEquals(40, personService.find("Alice").state);
+        Assert.assertEquals(30, personService.find("Bob").state);
+        Assert.assertEquals(50, personService.find("Carol").state);
     }
 }
--- transactions/postgre/src/main/java/ru/antivoland/sandbox/transactions/postgre/PersonService.java
+++ transactions/postgre/src/main/java/ru/antivoland/sandbox/transactions/postgre/PersonService.java
@@ -3,65 +3,40 @@
  */
 package ru.antivoland.sandbox.transactions.postgre;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Service
 public class PersonService {
-    private static final Logger log = LoggerFactory.getLogger(PersonService.class);
-
-    private static final RowMapper<Person> PERSON_MAPPER = new RowMapper<Person>() {
-        @Override
-        public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Person person = new Person();
-            person.id = rs.getString("id");
-            person.state = rs.getInt("state");
-            return person;
-        }
-    };
-
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    PersonRepository personRepository;
 
-    public void add(String id) {
-        log.info("Adding person " + id);
-        jdbcTemplate.update("INSERT INTO person(id) VALUES (?)", id);
+    public Person find(String id) {
+        return personRepository.find(id);
+    }
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
     }
 
     public void addAll(String... ids) {
-        for (String person : ids) {
-            add(person);
-        }
+        personRepository.addAll(ids);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.REPEATABLE_READ)
-    public void updateState(String id) {
-        Person person = find(id);
-        jdbcTemplate.update("UPDATE person SET state = ? WHERE id = ?", person.state + 1, id);
+    public void nextState(String id) {
+        Person person = personRepository.find(id);
+        personRepository.updateState(id, person.state + 1);
+        // do business
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    @Transactional(propagation = Propagation.SUPPORTS)
-    public Person find(String id) {
-        return jdbcTemplate.queryForObject("SELECT * from person WHERE id = ?", PERSON_MAPPER, id);
-    }
-
-    public List<Person> findAll() {
-        return jdbcTemplate.query("SELECT * from person", PERSON_MAPPER);
     }
 }
