e4eadcd74a81313d0b7b022ed737d53735415a85
antivoland <antivoland@inbox.ru>
2016-07-19 15:57:04 +0300

    RT expecting storage value to be versioned

--- rtest/src/main/java/antivoland/rtest/model/Storage.java
+++ rtest/src/main/java/antivoland/rtest/model/Storage.java
@@ -1,6 +1,6 @@
 package antivoland.rtest.model;
 
-public interface Storage<KEY, VALUE> {
+public interface Storage<KEY, VALUE extends Versioned> {
     VALUE get(KEY key);
 
     boolean insert(KEY key, VALUE value);
