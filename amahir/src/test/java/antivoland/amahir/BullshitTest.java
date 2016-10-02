55e98669b7572e1c8b71d2764a3a927de04df341
antivoland <antivoland@gmail.com>
2016-10-03 00:29:19 +0300

    AH implement bullshit routine

---
+++ amahir/src/test/java/antivoland/amahir/BullshitTest.java
@@ -0,0 +1,25 @@
+package antivoland.amahir;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BullshitTest {
+    @Test
+    public void testBullshit() throws Exception {
+        String phrase = "Quick win by Search Engine Optimization Policy";
+        Assert.assertEquals(2, Bullshit.nonbullshitability(phrase, dictionary()));
+    }
+
+    private Bullshit.Dictionary dictionary() throws Exception {
+        Path path = Paths.get(ClassLoader.getSystemResource("bullshit.txt").toURI());
+        try (Stream<String> stream = Files.lines(path)) {
+            return new Bullshit.Dictionary(stream.collect(Collectors.toSet()));
+        }
+    }
+}
