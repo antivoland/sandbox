7a073cc098ae56155cd9818b2eae74855ff2b3ae
antivoland <antivoland@gmail.com>
2016-10-17 10:03:43 +0300

    AH use rare names for test output

--- amahir/translit/src/test/java/antivoland/amahir/translit/TranslitTest.java
+++ amahir/translit/src/test/java/antivoland/amahir/translit/TranslitTest.java
@@ -2,6 +2,8 @@ package antivoland.amahir.translit;
 
 import io.github.adrianulbona.hmm.Emission;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,11 +16,12 @@ import java.util.stream.Stream;
 import static java.util.Arrays.asList;
 
 public class TranslitTest {
+    private static final Logger LOG = LoggerFactory.getLogger(TranslitTest.class);
     private static final String RU_REGEX = "[а-яА-Я]+\\t[0-9]+";
     private static final String EN_REGEX = "[a-zA-Z']+\\t[0-9]+";
 
     @Test
-    public void testEnRuShort() throws Exception {
+    public void testEnRu() throws Exception {
         List<Emission<TranslitState, TranslitObservation>> ruEnEmissions = ruEnEmissions();
         List<Emission<TranslitState, TranslitObservation>> enRuEmissions = enRuEmissions();
         TranslitModel model = new TranslitModel(
@@ -26,12 +29,20 @@ public class TranslitTest {
                 ruWordFrequency("names.stats"),
                 new TranslitSequencer(ruEnEmissions),
                 new TranslitSequencer(enRuEmissions));
-        String translit = model.translit("valdemar");
-        System.out.println("valdemar -> " + translit);
+        testEnRu("avundiy", model);
+        testEnRu("plakilla", model);
+        testEnRu("usfazan", model);
+        testEnRu("pheognia", model);
+        testEnRu("epiktet", model);
+    }
+
+    private void testEnRu(String enName, TranslitModel model) throws Exception {
+        String translit = model.translit(enName);
+        LOG.info("{} -> {}", enName, translit);
     }
 
     @Test
-    public void testRuEnShort() throws Exception {
+    public void testRuEn() throws Exception {
         List<Emission<TranslitState, TranslitObservation>> ruEnEmissions = ruEnEmissions();
         List<Emission<TranslitState, TranslitObservation>> enRuEmissions = enRuEmissions();
         TranslitModel model = new TranslitModel(
@@ -39,8 +50,16 @@ public class TranslitTest {
                 enWordFrequency("names.stats"),
                 new TranslitSequencer(enRuEmissions),
                 new TranslitSequencer(ruEnEmissions));
-        String translit = model.translit("вальдемар");
-        System.out.println("вальдемар -> " + translit);
+        testRuEn("авундий", model);
+        testRuEn("плакилла", model);
+        testRuEn("усфазан", model);
+        testRuEn("феогния", model);
+        testRuEn("эпиктет", model);
+    }
+
+    private void testRuEn(String ruName, TranslitModel model) throws Exception {
+        String translit = model.translit(ruName);
+        LOG.info("{} -> {}", ruName, translit);
     }
 
     private List<Emission<TranslitState, TranslitObservation>> ruEnEmissions() throws Exception {
