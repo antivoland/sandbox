b56928c1a7fa87b0f08bb6869ba089f6834a88fb
antivoland <antivoland@gmail.com>
2020-04-25 16:31:52 +0300

    init repo

---
+++ covid-19/src/main/java/antivoland/sandbox/covid19/Extractor.java
@@ -0,0 +1,15 @@
+package antivoland.sandbox.covid19;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author antivoland
+ */
+public class Extractor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Extractor.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello, Covid!");
+    }
+}
