42c1228f1a7a31371517968ba6c8c4637ad59ff4
Vladimir Pavlovskii <antivoland@gmail.com>
2023-03-22 12:31:52 +0200

    move glu test into separate repo

--- glutest/server/src/main/java/com/glu/glutest/App.java
+++
@@ -1,9 +0,0 @@
-package com.glu.glutest;
-
-import com.google.inject.Injector;
-import org.apache.commons.configuration.PropertiesConfiguration;
-
-public class App {
-    public static PropertiesConfiguration config;
-    public static Injector injector;
-}
--- glutest/server/src/main/java/com/glu/glutest/dao/ProfileDAO.java
+++
@@ -1,6 +0,0 @@
-package com.glu.glutest.dao;
-
-public interface ProfileDAO {
-    Profile get(String userId);
-    void save(Profile profile);
-}
